a3c59a23eb6553a97df0cb9b7f13c8c4947467a3
code4aLiving <sergio.nava89@gmail.com>
2017-03-03 11:11:30 +0000

    sd

--- mergePerformance.py
+++ mergePerformance.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+#import scipy.optimize as spo
 
 
 def get_data():
@@ -17,15 +17,41 @@ def time_spam_to_minutes(ts):
 		l = list(map(int,ts.split(':')))
 		return l[0]*60 + l[1] + l[2]/60.0
 
+def error_poly(coef, data):
+	"""
+	Computes the error between given polynomial and observed data
+
+	Parameters
+	coef: numpy.poly1D object or equivalent array representing polynomial coeficients
+	data: 2D array where each row is a point (x,y)
+	"""
+	err = np.sum((data[:,1] - np.polyval(coef,data[:,0]))**2)
+	return err
+
+def fit_poly(data, error_func, degree=3):
+	"""
+	Fit a polynomial to given data, using supplied error function
+	"""
+	Cguess = np.poly1d(np.ones(degree + 1, dtype=np.float32))
+
+	#Plot initial guess
+	x = np.linspace(-5,5,21)
+	plt.plot(x,np.polyval(Cguess,x),'r--', linewidth=2.0, label="Initial Guess")
+
+	#Call the optimizer
+	result = spo.minimize(error_func, Cguess, args=(data,), method='SLSQP',options={'disp':True})
+	return np.poly1d(result.x)
+
 if __name__=="__main__":
 	df = get_data()
-	df['TimeWithoutChecksum'] = df['TimeWithoutChecksum'].interpolate(method='quadratic')
-	print(df)
-	ax = df.plot(kind='line',title="Merging Suppliers",loglog=True,style='-o')
-	ax.set_xlabel("Suppliers")
-	ax.set_ylabel("Time in minutes")
-	lines,labels = ax.get_legend_handles_labels()
-	labels[0]="Improved"
-	labels[1] = "Current"
-	ax.legend(lines,labels)
-	plt.show()
+	poly1 = fit_poly(df.ix[:,['TimeChecksum']],error_poly)
+	print(df.ix[:,['TimeChecksum']])
+	#print(df)
+	#ax = df.plot(kind='line',title="Merging Suppliers",loglog=True,style='-o')
+	#ax.set_xlabel("Suppliers")
+	#ax.set_ylabel("Time in minutes")
+	#lines,labels = ax.get_legend_handles_labels()
+	#labels[0]="Improved"
+	#labels[1] = "Current"
+	#ax.legend(lines,labels)
+	#plt.show()

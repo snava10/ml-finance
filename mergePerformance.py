8829fdfa61ef3d40f96d031badcbdf51423cfef1
code4aLiving <sergio.nava89@gmail.com>
2017-05-07 21:13:54 +0100

    polishing portfolio manager

--- mergePerformance.py
+++ mergePerformance.py
@@ -15,7 +15,7 @@ def get_data():
 
 def time_spam_to_minutes(ts):
 	if isinstance(ts,str):
-		l = list(map(int,ts.split(':')))
+		l = list(map(float,ts.split(':')))
 		return l[0]*60 + l[1] + l[2]/60.0
 
 def error_poly(coef, data):
@@ -43,25 +43,78 @@ def fit_poly(data, error_func, degree=3):
 	result = spo.minimize(error_func, Cguess, args=(data,), method='SLSQP',options={'disp':True})
 	return np.poly1d(result.x)
 
-if __name__=="__main__":
-	df = get_data()
-	print(df.shape)
-	df['DimensionLines'] = df['DimensionLines']/df['DimensionLines'].max()
-	vals1 = df.ix[:df.shape[0]-1,['DimensionLines','TimeChecksum']].as_matrix()
-	poly1 = fit_poly(vals1,error_poly,degree=2)
-	print(poly1)
-	valsNotChecksum = df.ix[:df.shape[0]-3,['DimensionLines','TimeWithoutChecksum']].as_matrix()
-	print(valsNotChecksum)
-	polyNoChecksum = fit_poly(valsNotChecksum,error_poly,degree=2)
+def do_work():
+	#get data
+	df = pd.read_csv("data/testReport1.csv",
+		parse_dates=True,
+		usecols=["Description","Dimension Lines","Elapsed Time"])
+	df['Time'] = df['Elapsed Time'].apply(time_spam_to_minutes)
+	#print(df)
+
+	#normalise
+	normFactor = df['Dimension Lines'].max()
+	df['DimensionLines'] = df['Dimension Lines']/normFactor
+	#print(df)
+
+	dfNoChecksum = df[df["Description"]=="No checksum"][["DimensionLines","Time"]]
+	dfChecksum = df[df["Description"]=="checksum"][["DimensionLines","Time"]]
+	#print(dfNoChecksum)
+	#print(dfChecksum)
+
+	valsNoChecksum = dfNoChecksum.as_matrix()
+	polyNoChecksum = fit_poly(valsNoChecksum,error_poly,degree=2)
 	print(polyNoChecksum)
-	plt.plot(vals1[:,0]*10**6,vals1[:,1],'go',label="Improved")
-	plt.plot(valsNotChecksum[:,0]*10**6,valsNotChecksum[:,1],'bo',label="Current")
+
+	plt.figure(1)
+	plt.subplot(211)
+
+	plt.plot(valsNoChecksum[:,0],valsNoChecksum[:,1],'bo',label="Current")
+
+	valsChecksum = dfChecksum.as_matrix()
+	polyChecksum = fit_poly(valsChecksum,error_poly,degree=2)
+	print(polyChecksum)
+
+	plt.plot(valsChecksum[:,0],valsChecksum[:,1],'go',label="Improved")
+
 	p = np.array(range(0,100))
 	p = p/100
-	plt.plot(p*10**6,np.polyval(poly1,p),'m--', linewidth=1.0, label="Improved Estimation")
-	plt.plot(p*10**6,np.polyval(polyNoChecksum,p),'r--',linewidth=1.0, label="Current Estimation")
+	plt.plot(p,np.polyval(polyNoChecksum,p),'m--', linewidth=1.0, label="Current Estimation")
+	plt.plot(p,np.polyval(polyChecksum,p),'r--',linewidth=1.0, label="Improved Estimation")
 	plt.legend(loc='upper left')
-	plt.xlabel('Suppliers')
+	plt.xlabel('Suppliers x {0}'.format(normFactor))
 	plt.ylabel('Time in minutes')
 	plt.title('Merging suppliers time')
+
+	plt.subplot(212)
+	p = np.array(range(0,10**8,10**4))
+	plt.plot(p,np.polyval(polyNoChecksum,p)/60,'m--', linewidth=1.0, label="Current Estimation")
+	plt.plot(p,np.polyval(polyChecksum,p)/60,'r--',linewidth=1.0, label="Improved Estimation")
+	plt.legend(loc='upper left')
+	plt.xlabel('Suppliers x 100 M')
+	plt.ylabel('Time in hours')
 	plt.show()
+	return df
+
+if __name__=="__main__":
+	# df = get_data()
+	# print(df.shape)
+	# df['DimensionLines'] = df['DimensionLines']/df['DimensionLines'].max()
+	# vals1 = df.ix[:df.shape[0]-1,['DimensionLines','TimeChecksum']].as_matrix()
+	# poly1 = fit_poly(vals1,error_poly,degree=2)
+	# print(poly1)
+	# valsNotChecksum = df.ix[:df.shape[0]-3,['DimensionLines','TimeWithoutChecksum']].as_matrix()
+	# print(valsNotChecksum)
+	# polyNoChecksum = fit_poly(valsNotChecksum,error_poly,degree=2)
+	# print(polyNoChecksum)
+	# plt.plot(vals1[:,0]*10**6,vals1[:,1],'go',label="Improved")
+	# plt.plot(valsNotChecksum[:,0]*10**6,valsNotChecksum[:,1],'bo',label="Current")
+	# p = np.array(range(0,100))
+	# p = p/100
+	# plt.plot(p*10**6,np.polyval(poly1,p),'m--', linewidth=1.0, label="Improved Estimation")
+	# plt.plot(p*10**6,np.polyval(polyNoChecksum,p),'r--',linewidth=1.0, label="Current Estimation")
+	# plt.legend(loc='upper left')
+	# plt.xlabel('Suppliers')
+	# plt.ylabel('Time in minutes')
+	# plt.title('Merging suppliers time')
+	# plt.show()
+	do_work()
--- portfolio_statistics.py
+++ portfolio_statistics.py
@@ -3,26 +3,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statistics as st
 
-from data_reader import get_data, plot_data
+from data_reader import plot_data
 from statistics import compute_daily_returns
 import numpy as np
 import math
 import scipy.optimize as spo
 
 class Portfolio:
-	def __init__(self,startValue,symbols,startDate,endDate,allocation):
+
+	def __init__(self, startValue, symbols, startDate, endDate, allocation):
 		self.startValue = startValue
 		self.symbols = symbols
 		self.startDate = startDate
 		self.endDate = endDate
 		self.allocation = allocation
-		self.__loadData()
+		#self.rawdf = self._loadData(startDate, endDate)
+		#self.__loadData()
 	
+	def plot_daily_returns(self, symbols=None):
+		if not symbols:
+			symbols = self.symbols
+		df = Portfolio.get_data(symbols, self.startDate, self.endDate)
+		daily_returns = Portfolio.compute_daily_returns(df)
+		Portfolio.plot_data(daily_returns,title="Daily returns", xlabel="Dates", ylabel="Return")
+
+	def plot_raw(self):
+		df = Portfolio.get_data(self.symbols, self.startDate, self.endDate)
+		Portfolio.plot_data(df)
+
+	def plot_portfolio_raw(self, symbols=None):
+		if not symbols:
+			symbols = self.symbols
+		df = Portfolio.get_data(symbols, self.startDate, self.endDate)
+		Portfolio.plot_data(df.sum(axis=1), title="Total Portfolio Value")
+
+	def plot_portfolio_daily_returns(self, symbols=None):
+		if not symbols:
+			symbols=self.symbols
+		df = Portfolio.get_data(symbols, self.startDate, self.endDate)
+		df = pd.DataFrame(df.sum(axis=1),columns=['Portfolio'])
+		df = Portfolio.compute_daily_returns(df)
+		Portfolio.plot_data(df, title="Portfolio Daily Returns", xlabel="Dates", ylabel="Return")
+
 	def __loadData(self):
 		dates = pd.date_range(self.startDate, self.endDate)
 		df = get_data(self.symbols,dates)
 		df = df/df.ix[0,:] #normalizing
 		self.normalized_data = df
+		print(self.normalized_data)
 		for i in range(len(self.symbols)): #allocating
 			df[self.symbols[i]] = df[self.symbols[i]]*self.allocation[i]
 		
@@ -31,30 +59,28 @@ class Portfolio:
 		dr = (self.portVals / self.portVals.shift(1)) - 1
 		self.dailyReturns = dr[1:]
 
-	def get_cumulative_return(self):
-		return (self.portVals[-1]/self.portVals[0])-1
+	def get_basic_statistics(self, symbols=None):
+		if not symbols:
+			symbols = self.symbols
 
-	def get_average_daily_return(self):
-		return self.dailyReturns.mean()
+		df = Portfolio.get_data(symbols, self.startDate, self.endDate)
+		df = df.sum(axis=1)
+		#print(df)
+		netRet = df[0]-df[-1]
+		cumRet = Portfolio.get_cumulative_return(df)
 
-	def get_risk(self):
-		return self.dailyReturns.std()
+		df = pd.DataFrame(df,columns=['Portfolio'])
+		daily_returns = Portfolio.compute_daily_returns(df)
+		avgDailyRet = Portfolio.get_average_daily_return(daily_returns)[0]
 
-	def get_sharp_ratio(self, samplesPerYear=252, riskFreeRate=0):
-		k = math.sqrt(samplesPerYear)
-		dr = self.dailyReturns.copy() - riskFreeRate
-		return k * dr.mean()/self.dailyReturns.std()
-
-	def get_basic_statistics(self):
-		cumRet = self.get_cumulative_return()
-		avgDailyRet = self.get_average_daily_return()
-		risk = self.get_risk()
-		sr = self.get_sharp_ratio()
+		risk = Portfolio.get_risk(daily_returns)[0]
+		sr = Portfolio.get_sharp_ratio(daily_returns)[0]
 		res = { 
 			'Cumulative Return': cumRet,
 			'Average Daily Return': avgDailyRet,
 			'Risk': risk,
-			'Sharp Ratio': sr
+			'Sharp Ratio': sr,
+			'Net Return' : netRet
 		}
 		return res
 
@@ -77,12 +103,67 @@ class Portfolio:
 			constraints=constraint,method='SLSQP',options={'disp':True})
 		return result.x
 
+	def plot_portfolio(self):
+		plot_data(self.dailyReturns,title="Daily returns")
+
+	'''
+	Functions
+	'''
+	def compute_daily_returns(df):
+		daily_returns = df.copy()	
+		#daily_returns[1:] = (df[1:] / df[:-1].values) - 1 #using numpy
+		daily_returns = (df / df.shift(1)) - 1 # pure pandas
+		daily_returns.ix[0,:] = 0
+		return daily_returns
+
+	def get_cumulative_return(df):
+		return (df[-1]/df[0])-1
+
+	def get_average_daily_return(daily_returns):
+		return daily_returns.mean()
+
+	def get_risk(daily_returns):
+		return daily_returns.std()
+
+	def get_sharp_ratio(dailyReturns, samplesPerYear=252, riskFreeRate=0):
+		k = math.sqrt(samplesPerYear)
+		dr = dailyReturns.copy() - riskFreeRate
+		return k * dr.mean()/dailyReturns.std()
+	
+	def plot_data(df, title='Stock prices',xlabel="Date",ylabel="Price"):
+		ax = df.plot(title=title)
+		ax.set_xlabel(xlabel)
+		ax.set_ylabel(ylabel)
+		plt.show()
+
+	def get_data(symbols, startDate, endDate, include_spy=False):
+		"""Read stock data (adjusted close) for given symbols from CSV files."""
+		dates = pd.date_range(startDate, endDate)
+		df = pd.DataFrame(index=dates)
+		if include_spy and 'SPY' not in symbols:  # add SPY for reference, if absent
+			symbols.insert(0, 'SPY')
+
+		for symbol in symbols:
+			symbolDf = pd.read_csv("data/{}.csv".format(symbol.lower()),
+				index_col="Date",
+				parse_dates=True,
+				usecols=['Date','Adj Close'],
+				na_values=['nan'])
+			symbolDf = symbolDf.rename(columns={'Adj Close':symbol.upper()})
+			df = df.join(symbolDf, how='inner')
+		return df
+
 if __name__=="__main__":
-	dates = pd.date_range('2014-01-01', '2016-12-31')
+	#dates = pd.date_range('2014-01-01', '2016-12-31')
 	symbols = ['SPY','GOOGL','MSFT','YHOO']
 	portfolio = Portfolio(100000,symbols,'2014-01-01','2016-12-31',[0.4,0.4,0.1,0.1])
+	#print(portfolio.get_basic_statistics())
+	#print(portfolio.optimize_for_sharp_ratio())
+	#portfolio.plot_portfolio()
+
+	portfolio.plot_raw()
+	portfolio.plot_daily_returns()
+	portfolio.plot_daily_returns(symbols=['GOOGL'])
+	portfolio.plot_portfolio_raw()
+	portfolio.plot_portfolio_daily_returns()
 	print(portfolio.get_basic_statistics())
-	print(portfolio.optimize_for_sharp_ratio())
-
-
-
